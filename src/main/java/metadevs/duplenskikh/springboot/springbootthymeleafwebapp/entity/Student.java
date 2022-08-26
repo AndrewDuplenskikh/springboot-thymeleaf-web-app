05ecbae539ac1cd547809cb128a5aa6224f958ec
duplenskikh <ss_1o1@mail.ru>
2022-08-26 23:48:38 +0500

    initial commit

---
+++ src/main/java/metadevs/duplenskikh/springboot/springbootthymeleafwebapp/entity/Student.java
@@ -0,0 +1,60 @@
+package metadevs.duplenskikh.springboot.springbootthymeleafwebapp.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "phone")
+    private long phone;
+
+    public Student() {
+        super();
+    }
+
+    public Student(String name, String email) {
+        super();
+        this.name = name;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+}
